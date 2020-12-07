6390b7966a3e507e0088af647b47af68cad5b761
Raymond Huang <rayh@hey.com>
2020-12-07 00:21:36 -0800

    rustfmt

--- src/day07.rs
+++ src/day07.rs
@@ -1,18 +1,17 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::collections::HashSet;
-use std::collections::HashMap;
-use std::collections::VecDeque;
 
 #[allow(dead_code)]
-pub fn main() -> std::io::Result<()>  {
+pub fn main() -> std::io::Result<()> {
     let file = File::open("input/007.txt")?;
     let reader = BufReader::new(file);
 
-    let entries: HashMap<String, Vec<BagSpec>> = reader.lines()
-        .map(|s| parse_line(s.unwrap()))
-        .collect();
+    let entries: HashMap<String, Vec<BagSpec>> =
+        reader.lines().map(|s| parse_line(s.unwrap())).collect();
 
     dbg!(part_1("shiny gold bags", &entries));
     dbg!(part_2("shiny gold bags", &entries) - 1); // subtract 1 to not count the outside
@@ -20,11 +19,13 @@ pub fn main() -> std::io::Result<()>  {
     Ok(())
 }
 
-pub fn part_1(key: &str, entries: &HashMap<String,Vec<BagSpec>>) -> usize {
+pub fn part_1(key: &str, entries: &HashMap<String, Vec<BagSpec>>) -> usize {
     let mut reverse_lookup: HashMap<&str, HashSet<String>> = HashMap::new();
     for (bag_name, contains) in entries.iter() {
         for bag_spec in contains {
-            let set = reverse_lookup.entry(&bag_spec.name).or_insert(HashSet::new());
+            let set = reverse_lookup
+                .entry(&bag_spec.name)
+                .or_insert(HashSet::new());
             set.insert(bag_name.clone());
         }
     }
@@ -47,19 +48,19 @@ pub fn part_1(key: &str, entries: &HashMap<String,Vec<BagSpec>>) -> usize {
     parents.len()
 }
 
-pub fn part_2(key: &str, hsh: &HashMap<String,Vec<BagSpec>>) -> i32 {
+pub fn part_2(key: &str, hsh: &HashMap<String, Vec<BagSpec>>) -> i32 {
     let contained = hsh.get(key).expect("should have had a value");
     let mut count = 1;
     for item in contained.iter() {
         count = count + item.count * (part_2(&item.name, &hsh));
-    };
+    }
     count
 }
 
-#[derive(Hash,Clone,Debug,Eq,PartialEq)]
+#[derive(Hash, Clone, Debug, Eq, PartialEq)]
 pub struct BagSpec {
     name: String,
-    count: i32
+    count: i32,
 }
 
 pub fn parse_line(s: String) -> (String, Vec<BagSpec>) {
@@ -70,9 +71,15 @@ pub fn parse_line(s: String) -> (String, Vec<BagSpec>) {
 
     loop {
         match words.next() {
-            Some("contain") => { bag_name = bag_name.trim().to_string(); break},
-            Some(s) => { bag_name.push_str(s); bag_name.push(' ')},
-            None => panic!("Unexpected end-of-line")
+            Some("contain") => {
+                bag_name = bag_name.trim().to_string();
+                break;
+            }
+            Some(s) => {
+                bag_name.push_str(s);
+                bag_name.push(' ')
+            }
+            None => panic!("Unexpected end-of-line"),
         }
     }
 
@@ -84,18 +91,19 @@ pub fn parse_line(s: String) -> (String, Vec<BagSpec>) {
                 if count != 0 {
                     contains_bags.push(BagSpec {
                         name: name.clone() + "bags",
-                        count: count
+                        count: count,
                     });
                 }
                 count = 0;
                 name.clear();
-            },
-            s => {
-                match s.parse::<i32>() {
-                    Ok(c) => { count = c },
-                    Err(_) => { name.push_str(s); name.push(' ')}
-                }
             }
+            s => match s.parse::<i32>() {
+                Ok(c) => count = c,
+                Err(_) => {
+                    name.push_str(s);
+                    name.push(' ')
+                }
+            },
         }
     }
 
@@ -108,14 +116,22 @@ mod tests {
 
     #[test]
     fn day7_parse_group() {
-        let bag1 = BagSpec { name: "bright white bags".to_string(), count: 1};
-        let bag2 = BagSpec { name: "muted yellow bags".to_string(), count: 2};
+        let bag1 = BagSpec {
+            name: "bright white bags".to_string(),
+            count: 1,
+        };
+        let bag2 = BagSpec {
+            name: "muted yellow bags".to_string(),
+            count: 2,
+        };
 
+        let expected = ("light red bags".to_string(), vec![bag1, bag2]);
 
-        let expected = ("light red bags".to_string(), vec!(bag1, bag2));
-
-        assert_eq!(expected,
-            parse_line("light red bags contain 1 bright white bag, 2 muted yellow bags.".to_string())
+        assert_eq!(
+            expected,
+            parse_line(
+                "light red bags contain 1 bright white bag, 2 muted yellow bags.".to_string()
+            )
         );
     }
 }
