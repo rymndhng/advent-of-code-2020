fafe7776fa5fb34d419add4d1fa1a377944c3602
Raymond Huang <rayh@hey.com>
2020-12-07 23:07:25 -0800

    Cleanup

--- src/day08.rs
+++ src/day08.rs
@@ -3,17 +3,11 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-#[derive(Hash, Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug)]
 enum Cmd {
-    Nop,
-    Acc,
-    Jmp,
-}
-
-#[derive(Hash, Clone, Debug, Eq, PartialEq)]
-struct Command {
-    cmd: Cmd,
-    arg: i32,
+    Nop(i32),
+    Acc(i32),
+    Jmp(i32),
 }
 
 pub fn main() -> std::io::Result<()> {
@@ -34,27 +28,27 @@ pub fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn part_1(commands: &[Command]) -> Result<i32, (String, i32)> {
+fn part_1(commands: &[Cmd]) -> Result<i32, (String, i32)> {
     let mut acc: i32 = 0;
     let mut next_instruction: i32 = 0;
-    let mut previously_executed: HashSet<i32> = HashSet::new();
+    let mut executed_instructions: HashSet<i32> = HashSet::new();
 
     while let Some(cmd) = commands.get(next_instruction as usize) {
-        if previously_executed.contains(&next_instruction) {
+        if executed_instructions.contains(&next_instruction) {
             return Err((format!("ERROR: Looped at {}", next_instruction), acc));
         }
-        previously_executed.insert(next_instruction);
+        executed_instructions.insert(next_instruction);
 
-        match cmd.cmd {
-            Cmd::Acc => {
-                acc = acc + cmd.arg;
+        match cmd {
+            Cmd::Acc(arg) => {
+                acc = acc + arg;
                 next_instruction = next_instruction + 1;
             }
-            Cmd::Nop => {
+            Cmd::Nop(_) => {
                 next_instruction = next_instruction + 1;
             }
-            Cmd::Jmp => {
-                next_instruction = next_instruction + cmd.arg;
+            Cmd::Jmp(arg) => {
+                next_instruction = next_instruction + arg;
             }
         }
     }
@@ -62,34 +56,21 @@ fn part_1(commands: &[Command]) -> Result<i32, (String, i32)> {
     return Ok(acc);
 }
 
-fn part_2(commands: &[Command]) -> Result<i32, &str> {
-    for (idx, command) in commands.iter().enumerate() {
-        let mutated_commands = match command.cmd {
-            Cmd::Nop => {
-                let mut new_commands = Vec::with_capacity(commands.len());
-                new_commands.extend_from_slice(&commands[0..idx]);
-                new_commands.push(Command {
-                    cmd: Cmd::Jmp,
-                    arg: command.arg,
-                });
-                new_commands.extend_from_slice(&commands[idx + 1..]);
-                Some(new_commands)
-            }
-            Cmd::Jmp => {
-                let mut new_commands = Vec::with_capacity(commands.len());
-                new_commands.extend_from_slice(&commands[0..idx]);
-                new_commands.push(Command {
-                    cmd: Cmd::Nop,
-                    arg: command.arg,
-                });
-                new_commands.extend_from_slice(&commands[idx + 1..]);
-                Some(new_commands)
-            }
+fn part_2(commands: &[Cmd]) -> Result<i32, &str> {
+    for (idx, cmd) in commands.iter().enumerate() {
+        let mutated_command = match cmd {
+            Cmd::Nop(arg) => Some(Cmd::Jmp(*arg)),
+            Cmd::Jmp(arg) => Some(Cmd::Nop(*arg)),
             _ => None,
         };
 
-        if let Some(mutated_commands) = mutated_commands {
-            if let Ok(ret) = part_1(&mutated_commands) {
+        if let Some(mutated_command) = mutated_command {
+            let mut new_commands = Vec::with_capacity(commands.len());
+            new_commands.extend_from_slice(&commands[0..idx]);
+            new_commands.push(mutated_command);
+            new_commands.extend_from_slice(&commands[idx + 1..]);
+
+            if let Ok(ret) = part_1(&new_commands) {
                 return Ok(ret);
             }
         }
@@ -98,7 +79,7 @@ fn part_2(commands: &[Command]) -> Result<i32, &str> {
     return Err("Could not fix corrupted commands");
 }
 
-fn parse_line(s: &str) -> Command {
+fn parse_line(s: &str) -> Cmd {
     let (cmd, arg) = s.split_at(3);
 
     let cmd = match cmd {
@@ -113,12 +94,12 @@ fn parse_line(s: &str) -> Command {
     let sign = match chars.next() {
         Some('+') => 1,
         Some('-') => -1,
-        _ => unreachable!()
+        _ => unreachable!(),
     };
 
     let arg = chars.collect::<String>().parse::<i32>().unwrap() * sign;
 
-    Command { cmd, arg }
+    cmd(arg)
 }
 
 #[cfg(test)]
@@ -127,17 +108,7 @@ mod tests {
 
     #[test]
     fn day8_parse_line() {
-        let expected = Command {
-            cmd: Cmd::Nop,
-            arg: 0,
-        };
-        assert_eq!(expected, parse_line("nop +0"));
-        assert_eq!(
-            Command {
-                cmd: Cmd::Acc,
-                arg: 1
-            },
-            parse_line("acc +1")
-        );
+        assert_eq!(Cmd::Nop(0), parse_line("nop +0"));
+        assert_eq!(Cmd::Acc(1), parse_line("acc +1"));
     }
 }
