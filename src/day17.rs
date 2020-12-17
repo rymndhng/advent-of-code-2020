0dd509aec5b00291426dde55acd8671019584790
Raymond Huang <rayh@hey.com>
2020-12-17 12:39:33 -0800

    Day 17: Playing with Traits

--- src/day17.rs
+++ src/day17.rs
@@ -1,114 +1,120 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs;
+use std::hash::Hash;
 
 pub fn main() -> std::io::Result<()> {
     let input = fs::read_to_string("input/17.txt")?;
+    dbg!(part_1(&input));
     dbg!(part_2(&input));
-
     Ok(())
 }
 
+fn part_1(grid: &str) -> usize {
+    let cycles = 6;
+    let mut grid = Point3::parse(grid);
+    println!("iter: {}, count: {}", 0, grid.len());
+    for cycle in 1..=cycles {
+        grid = next_state(&grid);
+        println!("iter: {}, count: {}", cycle, grid.len());
+    }
+    grid.len()
+}
+
+// (5 29 60 320 188 1056 848)
 fn part_2(grid: &str) -> usize {
     let cycles = 6;
-    let (mut grid, mut dim) = parse(grid);
-    println!(
-        "iter: {}, count: {}",
-        0,
-        grid.iter().filter(|(_, &active)| active).count()
-    );
-
+    let mut grid = Point4::parse(grid);
+    println!("iter: {}, count: {}", 0, grid.len());
     for cycle in 1..=cycles {
-        grid = next_state(&grid, dim);
-        println!(
-            "iter: {}, dim: {}, count: {}",
-            cycle,
-            dim,
-            grid.iter().filter(|(_, &active)| active).count()
-        );
-        // dbg!(&grid);
-        dim += 1;
+        grid = next_state(&grid);
+        println!("iter: {}, count: {}", cycle, grid.len());
     }
-
-    grid.iter().filter(|(_, &active)| active).count()
+    grid.len()
 }
 
-type Point = (i32, i32, i32, i32);
+fn next_state<A: Clone + Hash + Eq + ConwayCube>(grid: &HashSet<A>) -> HashSet<A> {
+    let frequencies =
+        grid.iter()
+            .flat_map(|p| p.neighbors())
+            .fold(HashMap::new(), |mut acc, point| {
+                let counter = acc.entry(point).or_insert(0);
+                *counter += 1;
+                acc
+            });
 
-fn parse(grid: &str) -> (HashMap<Point, bool>, i32) {
-    let mut points: HashMap<Point, bool> = HashMap::new();
-    let dim = grid.lines().next().unwrap().len();
-    let dim = (dim / 2) as i32;
-
-    for (y, row) in grid.lines().enumerate() {
-        for (x, on) in row.chars().enumerate() {
-            let point = (x as i32 - dim, y as i32 - dim, 0, 0);
-            points.insert(point, on == '#');
-        }
-    }
-
-    (points, dim as i32 + 2)
+    frequencies
+        .iter()
+        .filter(|(p, &count)| (count == 3) || (grid.contains(p) && count == 2))
+        .map(|(p, _)| p.clone())
+        .collect()
 }
 
-fn next_state(grid: &HashMap<Point, bool>, dim: i32) -> HashMap<Point, bool> {
-    let mut next_state: HashMap<Point, bool> = HashMap::new();
+trait ConwayCube {
+    fn neighbors(&self) -> HashSet<Self>
+    where
+        Self: std::marker::Sized;
 
-    for point in grid_points(dim).iter() {
-        let active = match grid.get(point).unwrap_or(&false) {
-            true => [2, 3].contains(
-                &surrounding_points(*point)
-                    .iter()
-                    .filter(|p| *grid.get(p).unwrap_or(&false))
-                    .count(),
-            ),
-            false => {
-                3 == surrounding_points(*point)
-                    .iter()
-                    .filter(|p| *grid.get(p).unwrap_or(&false))
-                    .count()
-            }
-        };
-
-        next_state.insert(*point, active);
-    }
-    next_state
+    fn parse(grid: &str) -> HashSet<Self>
+    where
+        Self: std::marker::Sized;
 }
 
-fn grid_points(dim: i32) -> HashSet<Point> {
-    let mut points = HashSet::new();
-    for x in -dim..dim {
-        for y in -dim..dim {
-            for z in -dim..dim {
-                for w in -dim..dim {
-                    points.insert((x, y, z, w));
+type Point3 = (i32, i32, i32);
+
+lazy_static! {
+    static ref RANGE: [i32; 3] = [-1, 0, 1];
+}
+
+impl ConwayCube for Point3 {
+    fn neighbors(&self) -> HashSet<Point3> {
+        // HOW to implement an iterator instead?
+        RANGE
+            .iter()
+            .flat_map(|x| RANGE.iter().map(move |y| (x, y)))
+            .flat_map(|(x, y)| RANGE.iter().map(move |z| (x, y, z)))
+            .map(|(x, y, z)| (self.0 + x, self.1 + y, self.2 + z))
+            .filter(|&p| p != *self)
+            .collect()
+    }
+
+    fn parse(grid: &str) -> HashSet<Point3> {
+        let mut points: HashSet<Point3> = HashSet::new();
+        for (y, row) in grid.lines().enumerate() {
+            for (x, c) in row.chars().enumerate() {
+                if c == '#' {
+                    let point = (x as i32, y as i32, 0);
+                    points.insert(point);
                 }
             }
         }
+        points
     }
-    dbg!(&points.len());
-    points
 }
 
-fn surrounding_points(point: Point) -> HashSet<Point> {
-    let (x, y, z, w) = point;
-    let mut surrounding = HashSet::new();
+type Point4 = (i32, i32, i32, i32);
+impl ConwayCube for Point4 {
+    fn neighbors(&self) -> HashSet<Point4> {
+        RANGE
+            .iter()
+            .flat_map(|x| RANGE.iter().map(move |y| (x, y)))
+            .flat_map(|(x, y)| RANGE.iter().map(move |z| (x, y, z)))
+            .flat_map(|(x, y, z)| RANGE.iter().map(move |w| (x, y, z, w)))
+            .map(|(x, y, z, w)| (self.0 + x, self.1 + y, self.2 + z, self.3 + w))
+            .filter(|&p| p != *self)
+            .collect()
+    }
 
-    let xs = [x - 1, x, x + 1];
-    let ys = [y - 1, y, y + 1];
-    let zs = [z - 1, z, z + 1];
-    let ws = [w - 1, w, z + 1];
-
-    for &x in &xs {
-        for &y in &ys {
-            for &z in &zs {
-                for &w in &ws {
-                    if (x, y, z, w) != point {
-                        surrounding.insert((x, y, z, w));
-                    }
+    fn parse(grid: &str) -> HashSet<Point4> {
+        let mut points: HashSet<Point4> = HashSet::new();
+        for (y, row) in grid.lines().enumerate() {
+            for (x, c) in row.chars().enumerate() {
+                if c == '#' {
+                    let point = (x as i32, y as i32, 0, 0);
+                    points.insert(point);
                 }
             }
         }
+        points
     }
-
-    surrounding
 }
